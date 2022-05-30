de5e13f89dabb768273a44b2e22d592e202279cd
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-05-30 13:55:17 +0200

    Add some enemys

--- BasicGame.ConsoleGame/Entities/Creatures/Creature.cs
+++ BasicGame.ConsoleGame/Entities/Creatures/Creature.cs
@@ -1,10 +1,20 @@
 ﻿internal class Creature : IDrawable
 {
     private Cell cell;
+    private int health;
     public string Symbol { get; }
     public ConsoleColor Color { get; protected set; } = ConsoleColor.Green;
 
-    public int Health { get; internal set; }
+    public int Health 
+    { 
+        get => health < 0 ? 0 : health; 
+        set => health = value >= MaxHealth ? MaxHealth : value;
+    }
+
+    public int Damage { get; set; } = 50;
+    public bool IsDead => health <= 0;
+
+    public int MaxHealth { get; }
     public Cell Cell 
     { 
         get => cell;
@@ -16,11 +26,12 @@
     
     }
 
-    public Creature(Cell cell, string symbol)
+    public Creature(Cell cell, string symbol, int maxHealth)
     {
         ArgumentNullException.ThrowIfNull(nameof(cell));
         this.cell = cell;
         Symbol = symbol;
+        MaxHealth = maxHealth;
     }
 
 
---
+++ BasicGame.ConsoleGame/Entities/Creatures/Goblin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicGame.ConsoleGame.Entities.Creatures
+{
+    internal class Goblin : Creature
+    {
+        public Goblin(Cell cell, int maxHealth) : base(cell, "G ", maxHealth)
+        {
+            Damage = 15;
+            Color = ConsoleColor.DarkBlue;
+        }
+    } 
+    
+    internal class Orc : Creature
+    {
+        public Orc(Cell cell, int maxHealth) : base(cell, "O ", maxHealth)
+        {
+            Damage = 40;
+            Color = ConsoleColor.DarkGreen;
+        }
+    }
+    
+    internal class Troll : Creature
+    {
+        public Troll(Cell cell, int maxHealth) : base(cell, "T ", maxHealth)
+        {
+            Damage = 30;
+            Color = ConsoleColor.DarkYellow;
+        }
+    }
+}
